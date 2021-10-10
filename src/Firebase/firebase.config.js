ee1bcc01b7022afa97499ef65b621f69052d61c0
Lukman Hossain <lukman.webdev@gmail.com>
2021-10-10 18:36:11 +0600

    project setup

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA3IwATClFSTQ3gC7gQpVZ4c_nbDLmhniM",
+    authDomain: "ema-jhon-simple-e831b.firebaseapp.com",
+    projectId: "ema-jhon-simple-e831b",
+    storageBucket: "ema-jhon-simple-e831b.appspot.com",
+    messagingSenderId: "861746663072",
+    appId: "1:861746663072:web:56279c2310d0c9f9fac3c5"
+};
+
+export default firebaseConfig;
